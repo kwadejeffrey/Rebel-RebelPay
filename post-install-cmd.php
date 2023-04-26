69d792e12d685ca371fbe3ca247ed2e0a0e6f16c
RebelNii <RebelNii@users.noreply.github.com>
2023-04-26 12:33:31 +0000

    Fix styling

--- post-install-cmd.php
+++ post-install-cmd.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!isset($_ENV['CI']) && !isset($_SERVER['CI'])) {
-    $starRepo = readline("Thank you for installing the package! Would you like to star our GitHub repository? (Y/n): ");
+if (! isset($_ENV['CI']) && ! isset($_SERVER['CI'])) {
+    $starRepo = readline('Thank you for installing the package! Would you like to star our GitHub repository? (Y/n): ');
 
     if (strtolower($starRepo) === 'y') {
         echo "Opening repository page...\n";
