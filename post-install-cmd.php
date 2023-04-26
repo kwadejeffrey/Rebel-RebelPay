2143a3a196d7339e5eb0359ff03860f439ff7135
Kwade Jeffrey <kwadejeffrey@gmail.com>
2023-04-26 12:19:21 +0000

    README.md Update

---
+++ post-install-cmd.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!isset($_ENV['CI']) && !isset($_SERVER['CI'])) {
+    $starRepo = readline("Thank you for installing the package! Would you like to star our GitHub repository? (Y/n): ");
+
+    if (strtolower($starRepo) === 'y') {
+        echo "Opening repository page...\n";
+        exec('start https://github.com/RebelNii/Rebel-RebelPay');
+    }
+}
